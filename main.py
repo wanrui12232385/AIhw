cc603dbaad52002c367e03fb76ea3ea8209db400
wanrui12232385 <113428740+wanrui12232385@users.noreply.github.com>
2022-12-24 15:19:48 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import torch
+import numpy as np
+import random
+from torch.utils.data import Dataset
+import soundfile as sf
+
+root = "/Users/wangrui/Downloads/LibriSpeech-SI/train"
+
+class MyDataset(Dataset):
+
+    def __init__(self, voice, labels, transform=None, target_transform=None):
+
+        self.voice = voice
+        self.labels = labels
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.voice)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        path = self.voice[idx]
+        target = self.labels[idx]
+        voice = sf.read(path)
+
+        if self.transform:
+            voice = self.transform(voice)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return voice, target
+
+
+
